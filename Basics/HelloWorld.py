007b753f5aa18cd0538c50f76f303e3719af7e8d
Mariusz Gruszczynski <mariuszgruszczynski11@gmail.com>
2022-09-01 20:18:56 +0200

    Operators test 2

--- Basics/HelloWorld.py
+++ Basics/HelloWorld.py
@@ -6,3 +6,13 @@ cube = 3 ** 3
 print(operators)
 print(square)
 print(cube)
+
+text1 = "Hello" + " " + "World"
+text2 = "Hello" * 3
+
+even = [2, 4, 6]
+odd = [1, 3, 5]
+evenodd = even + odd
+
+
+print(text1, text2, evenodd)
