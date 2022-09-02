d07a719654e3998c77486f9075a2b26a8b5859c2
Mariusz Gruszczynski <mariuszgruszczynski11@gmail.com>
2022-09-02 21:09:49 +0200

    Variable arguments test

--- Basics/HelloWorld.py
+++ Basics/HelloWorld.py
@@ -1,5 +1,14 @@
-sentence = "A long time ago in a galaxy far far away"
-words = sentence.split()
+def func1(one, two, *rest):
+    print("%s %s %s" % (one, two, rest))
 
-wordsLengths = [len(word) for word in words if word != "far"]
-print(wordsLengths)
+def func2(one, two, **rest):
+    if rest.get("first") == True:
+        print(one)
+    if rest.get("second") == True:
+        print(two)
+
+
+func1(1, 2, 3, 4, 5)
+func1(2, "two", "a", "b", 4.3)
+
+func2(1, 2, first = True, second = True)
