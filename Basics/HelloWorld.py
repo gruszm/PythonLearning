01326ecc89683f0d707bde8dd5a734336316b1b6
Mariusz Gruszczynski <mariuszgruszczynski11@gmail.com>
2022-09-02 20:47:43 +0200

    Comprehension list test

--- Basics/HelloWorld.py
+++ Basics/HelloWorld.py
@@ -1,25 +1,5 @@
-dic = {}
-dic["One"] = 1
-dic["Two"] = 2
-dic["Three"] = 3
+sentence = "A long time ago in a galaxy far far away"
+words = sentence.split()
 
-dic_simple = {
-    "One" : 1,
-    "Two" : 2,
-    "Three" : 3
-}
-
-print(dic)
-print()
-print(dic_simple)
-print()
-
-for name, value in dic.items():
-    print("%d is %s" % (value, name))
-
-print()
-print(dic_simple["Two"])
-
-dic_simple.pop("Two")
-print()
-print(dic_simple)
+wordsLengths = [len(word) for word in words if word != "far"]
+print(wordsLengths)
