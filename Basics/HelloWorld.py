90ca7dbdbb6c0b6e7b5fed0e4dc2cd13ca3732b9
Mariusz Gruszczynski <mariuszgruszczynski11@gmail.com>
2022-09-02 12:45:31 +0200

    Loops 2

--- Basics/HelloWorld.py
+++ Basics/HelloWorld.py
@@ -8,3 +8,15 @@ counter = 10
 while counter > 0:
     print(counter)
     counter -= 1
+
+for x in range(11):
+    print(x)
+    if x == 5:
+        break
+
+for x in range(11):
+    if x % 2 == 0:
+        continue
+    else:
+        print(x)
+
