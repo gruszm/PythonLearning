c8d7d71d6fb030478bf14ef9774b1b8a9d44aefa
Mariusz Gruszczynski <mariuszgruszczynski11@gmail.com>
2022-09-03 15:58:30 +0200

    Set test

--- Basics/HelloWorld.py
+++ Basics/HelloWorld.py
@@ -1,14 +1,4 @@
-def func1(one, two, *rest):
-    print("%s %s %s" % (one, two, rest))
+testSet = set([1, 2, 3, 4, 1, 2, 5, 2])
 
-def func2(one, two, **rest):
-    if rest.get("first") == True:
-        print(one)
-    if rest.get("second") == True:
-        print(two)
-
-
-func1(1, 2, 3, 4, 5)
-func1(2, "two", "a", "b", 4.3)
-
-func2(1, 2, first = True, second = True)
+for x in testSet:
+    print(x)
