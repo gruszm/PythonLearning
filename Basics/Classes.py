24ea038dd1d71e8e5589e5c09b34ff84956bf67f
Mariusz Gruszczynski <mariuszgruszczynski11@gmail.com>
2022-09-11 14:28:46 +0200

    Constructor, methods

--- Basics/Classes.py
+++ Basics/Classes.py
@@ -1,8 +1,24 @@
 class myClass:
     variable = "duh"
 
-    def func(self):
-        print("I say %s" % self.variable)
+    def __init__(self, variable2):
+        self.variable2 = variable2
 
-obj = myClass()
+    def func(self):
+        print("I say %s and %s" % (self.variable, self.variable2))
+
+class Person:
+    def __init__(self, firstName, lastName):
+        self.firstName = firstName
+        self.lastName = lastName
+
+    def introduce(self):
+        print("Hello, my name is %s %s" % (self.firstName, self.lastName))
+
+obj = myClass("blah")
 obj.func()
+
+p = Person("Mike", "Hawk")
+p.introduce()
+
+del obj, p
