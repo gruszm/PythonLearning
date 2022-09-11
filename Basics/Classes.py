e5874c2eefbb5d7477f1c640317b2aba3f7e2024
Mariusz Gruszczynski <mariuszgruszczynski11@gmail.com>
2022-09-11 15:49:47 +0200

    Inheritance

--- Basics/Classes.py
+++ Basics/Classes.py
@@ -15,10 +15,28 @@ class Person:
     def introduce(self):
         print("Hello, my name is %s %s" % (self.firstName, self.lastName))
 
+class Employee(Person):
+    def __init__(self, firstName, lastName, salary):
+        super().__init__(firstName, lastName)
+        self.salary = salary
+
+    def introduce(self):
+        super().introduce()
+        print("My salary is %s" % self.salary)
+
 obj = myClass("blah")
 obj.func()
+
+print()
 
 p = Person("Mike", "Hawk")
 p.introduce()
 
-del obj, p
+print()
+
+em = Employee("Jake", "Smith", "10000")
+em.introduce()
+
+print()
+
+del obj, p, em
