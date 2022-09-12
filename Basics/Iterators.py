a3ba466cd44fd9d92e3cf517888f96ba677c28d0
gruszm <mariuszgruszczynski11@gmail.com>
2022-09-12 20:37:24 +0200

    Iterators test

---
+++ Basics/Iterators.py
@@ -0,0 +1,16 @@
+tup = (1, 2, 3, 4, 5)
+
+it = iter(tup)
+
+print(next(it))
+print(next(it))
+print(next(it))
+
+print()
+
+tup2 = (2, 4, 6, 8, 10)
+
+it2 = tup2.__iter__()
+
+print(it2.__next__())
+print(it2.__next__())
